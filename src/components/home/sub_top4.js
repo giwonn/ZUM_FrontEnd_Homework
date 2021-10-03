6690a70ee9db116665a0912a7af317495979a06c
Giwon Lim <l4279625@gmail.com>
2021-10-04 00:02:31 +0900

    issue(#2): async/await 수정중... (promise 벗기자)

--- src/components/home/sub_top4.js
+++ src/components/home/sub_top4.js
@@ -1,11 +1,30 @@
 import Component from "../core/Component";
 
-class Top4 extends Component {
-    template() {
-        const { cnt } = this._state;
-        return `
-            <ul class="sublist" data-value='${cnt}'>
-            </ul>
-        `;
+class Sub_Top4 extends Component {
+    async render_ul(category, idx) {
+        const content = await fetch(`http://localhost:3000/api/content/${category}`).then(resp => resp.json());
+        let ul = `<ul data-idx=${idx}>`;
+
+        for (let i=0; i<4; i++) {
+            ul += `
+            <li>
+                <img src='${content[i].imageUrl}'>
+                <p>${content[i].title}</p>
+                <p>${content[i].summaryContent}</p>
+                <p>${content[i].mediaName}</p>
+            </li>
+            `;
+        }
+        ul += '</ul>';
+        return ul;
+    }
+
+    async template() {
+        const { category } = this._state;
+        const ul = await Promise.all(category.map((cate, idx) => this.render_ul(cate, idx)));
+        
+        return ul.join('');
     }
 }
+
+export default Sub_Top4;
