9b53d5c27260ff8787d06f396f685df189722dc4
Giwon Lim <l4279625@gmail.com>
2021-10-05 08:51:26 +0900

    feat(getApi.js): 상세페이지 api 호출 구현 완료

--- src/server/getApi.js
+++ src/server/getApi.js
@@ -1,3 +1,6 @@
+const e = require('express');
+const playwright = require('playwright');
+const chromium = playwright.chromium;
 
 const getApi = (app) => {
     const getList = (filePath) => {
@@ -5,28 +8,42 @@ const getApi = (app) => {
         try {
             data = require(filePath);
         } catch (err) {
-            alert('데이터를 불러오지 못했습니다.');
+            console.log('데이터를 불러오지 못했습니다.');
         }
         return data;
     }
     
     // 랭킹 리스트 호출
-    app.get('/api/best', (req, res) => res.json(getList('../api/best/ranking.json')));
+    app.get('/api/best', (req, resp) => resp.json(getList('../api/best/ranking.json')));
     
     // 카테고리 리스트 호출
-    app.get('/api/content/:category', (req, res) => res.json(getList(`../api/content/${req.params.category}.json`)));
+    app.get('/api/content/:category', (req, resp) => resp.json(getList(`../api/content/${req.params.category}.json`)));
     
     // 상세페이지 api 구현해야함 (미완성)
-    app.get('/api/content/:category/:idx', (req, res) => {
-        const category = req.params.category;
-        const idx = req.params.idx;
-        let HubContent = [];
-        try {
-            HubContent = res.json(require(`../api/content/${category}.json`));
-        } catch (err) {
-            console.log(err);
+    app.get('/api/detail/:url', async (req, resp) => {
+        const url = req.params.url;
+        let contents = {};
+
+        const browser = await chromium.launch();
+        const page = await browser.newPage();
+        await page.goto(`https://hub.zum.com/post/${url}`);
+
+        const post = await page.$$('.d_container');
+
+        for (let element of post) {
+            const article_header = await element.$('.article_header');
+            const d_article = await element.$('.d_article');
+            
+            const title = await article_header.innerHTML();
+            const content = await d_article.innerHTML();
+
+            contents.title = title;
+            contents.content = content;
         }
-        return data;
+
+        await browser.close();
+        
+        return resp.send(contents);
     });
 };
 
