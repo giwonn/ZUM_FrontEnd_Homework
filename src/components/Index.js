d4578a87ca273ab73e4520b4ae2fc4426e61e72b
Giwon Lim <l4279625@gmail.com>
2021-10-05 08:47:40 +0900

    feat(Index.js): Index.js를 메인페이지로 변경

---
+++ src/components/Index.js
@@ -0,0 +1,28 @@
+import Component from './core/Component';
+import Header from './common/Header';
+import Home from './Home';
+
+import '../css/home.css';
+import '../css/header.css';
+
+class Index extends Component {
+
+    template() {
+        return `
+        <div id='header'></div>
+    
+        <div id="contents"></div>
+        `;
+    }
+
+    async mounted() {
+        const header = this._target.querySelector('#header');
+        const contents = this._target.querySelector('#contents');
+
+        new Header(header);
+        new Home(contents);
+    }
+
+}
+
+export default Index;
