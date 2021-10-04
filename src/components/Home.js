412e733490b166c2a987002b774cf788c0bdd89d
Giwon Lim <l4279625@gmail.com>
2021-10-05 08:49:33 +0900

    refactor(Home.js): Index.js로 메인페이지 변경

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,43 +1,26 @@
 import Component from './core/Component';
-import Header from './common/Header';
 import Sub_Top4 from './home/Sub_Top4';
+import Top12 from './home/Top12';
 
-import '../css/home.css';
-import '../css/header.css';
 import '../css/sub_top4.css';
 import '../css/top12.css';
-import Top12 from './home/Top12';
 
 class Home extends Component {
 
     template() {
-        const { title, description } = this._state;
         return `
-        <div id='header'></div>
-    
-        <div id="contents">
             <div id='sub_top4'>
             </div>
 
             <div id="top12"></div>
-        </div>
         `;
     }
 
-    setEvent() {
-        this._target.querySelector('.title').addEventListener('click', (e) => {
-            alert("이벤트 발생");
-        });
-    }
-
     async mounted() {
-        const header = this._target.querySelector('#header');
         const sub_top4 = this._target.querySelector('#sub_top4');
         const top12 = this._target.querySelector('#top12');
-        const category = ['life', 'food', 'travel', 'culture'];
 
-        new Header(header);
-        new Sub_Top4(sub_top4, {category: category});
+        new Sub_Top4(sub_top4); // 카테고리 별로 탑4를 뽑아줌
         new Top12(top12);
     }
 
